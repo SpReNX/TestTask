01797acc8cad761b638571657bfd62dcdc3c25d3
sprenx <sprenx@ya.ru>
2024-04-15 17:55:00 +0300

    Добавлены запросы и примеры экспорта

---
+++ Запросы/EmployeesFilling.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO Employees (LastName, FirstName, MiddleName, BirthDate, PassportSeries, PassportNumber) 
+				VALUES ('Кошкин','Артем','Леонидович','2001-05-03', '2115', '123456');
+INSERT INTO Employees (LastName, FirstName, MiddleName, BirthDate, PassportSeries, PassportNumber) 
+				VALUES ('Иванов','Арсений','Алексеевич','1999-11-23', '2113', '464500');
+INSERT INTO Employees (LastName, FirstName, MiddleName, BirthDate, PassportSeries, PassportNumber) 
+				VALUES ('Мухин','Владимир','Юрьевич','2003-05-22', '2118', '124646');
---
+++ Запросы/OrganizationsFilling.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO Organizations (Name, INN, LegalAddress, ActualAddress) 
+				VALUES ('АО "Сбербанк"', '7708004767', 'г. Москва, ул.Правая, д. 32, офис 142', 'г. Москва, ул. Автозаводская, д. 111');
+INSERT INTO Organizations (Name, INN, LegalAddress, ActualAddress) 
+				VALUES ('ООО "Зеленоглазое такси"', '1027700092661', 'г. Москва, ул.Пушкина, д. 9, офис 23', 'г. Москва, ул. Маршала Полубоярова, д. 312');
+INSERT INTO Organizations (Name, INN, LegalAddress, ActualAddress) 
+				VALUES ('ЗАО "Психбольница N3"', '7839078339', 'МО, г. Люберцы, ул.Калинина, д. 1, офис 11', 'г. Пермь, ул. Ленина, д. 3');
